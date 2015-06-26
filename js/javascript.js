30f14e89cfe4e939e577910fbc2b1f6d5b616fdd
Imtaek Hong <imtaekh@gmail.com>
2015-06-25 17:20:20 -0700

    edited namePicker()

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,17 @@
 var wdi_17_names = ["Addison", "Ben", "Christine", "Chloe", "Cris", "Dane", "David H", "David Z", "Emily", "Imtaek", "Jack", "Jesse P", "Jesse Z", "Jonathan", "Pericles", "Peter", "Philippe", "Sergio", "Simon", "Tali", "Tavo", "Trevor"];
-
 var copiedNames = [];
 
-function exchange(arry, a, b){
-  var temp=arry[a];
-  arry[a]=arry[b];
-  arry[b]=temp;
+function namePicker(){
+  if(copiedNames.length === 0) copiedNames = wdi_17_names.slice();
+  var lastNum=copiedNames.length-1;
+  var rand=Math.floor(Math.random()*lastNum+1);
+  console.log(copiedNames[rand]);
+  copiedNames.splice(rand,1);
 }
 
+/*
 function namePicker(){
-  if(copiedNames.length === 0){
-    copiedNames = wdi_17_names.slice();
-  }
+  if(copiedNames.length === 0) copiedNames = wdi_17_names.slice();
   var lastNum=copiedNames.length-1;
   var rand=Math.floor(Math.random()*lastNum+1);
 
@@ -19,3 +19,4 @@ function namePicker(){
 
   console.log(copiedNames.pop());
 }
+//*/
